01afac3eaad1463842cf56ae66e24e3cebd9a782
Jamie Seward <jamie.seward@gmail.com>
2024-08-08 15:17:46 -0600

    added jobs-view component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { JobsViewComponent } from './jobs-view/jobs-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: JobsViewComponent}
+];
---
+++ src/app/jobs-view/jobs-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-jobs-view",
+    templateUrl: './jobs-view.component.html',
+    styleUrl: './jobs-view.component.css'
+})
+export class JobsViewComponent {
+
+}
