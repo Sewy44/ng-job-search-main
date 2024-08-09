65eed5672dd8e964e76cf3b5b584476772cd8aef
Jamie Seward <jamie.seward@gmail.com>
2024-08-09 15:29:40 -0600

    Unit tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,58 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink, ActivatedRoute } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let mockActivatedRoute;
+
   beforeEach(async () => {
+
+    mockActivatedRoute = {
+      snapshot: {
+        paramMap: {
+          get: (key: string) => 'test-value' // Mock implementation of paramMap.get
+        }
+      }
+    };
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [RouterOutlet, RouterLink, CommonModule, AppComponent], 
+      providers: [{provide: ActivatedRoute, useValue: mockActivatedRoute}]
     }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'ng-job-search' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-job-search');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ng-job-search');
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have the title 'ng-job-search'`, () => {
+    expect(component.title).toEqual('ng-job-search');
+  });
+
+  it('should set the initial active button to "jobs"', () => {
+    expect(component.activeButton).toBe('jobs');
+  });
+
+  it('should change activeButton when setActiveButton is called', () => {
+    component.setActiveButton('companies');
+    expect(component.activeButton).toBe('companies');
+  });
+
+  it('should call setActiveButton on ngOnInit', () => {
+    const setActiveButtonSpy = spyOn(component, 'setActiveButton');
+    component.ngOnInit();
+    expect(setActiveButtonSpy).toHaveBeenCalledWith('jobs');
+  });
+
+  it('should render the router outlet', () => {
+    const routerOutletDebugElement = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(routerOutletDebugElement).not.toBeNull();
   });
 });
